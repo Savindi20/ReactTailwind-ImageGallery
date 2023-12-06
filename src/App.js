697a12a03652e7b34fe24286dfba5a3d08da367d
Savindi Dadallage <121780084+Savindi20@users.noreply.github.com>
2023-12-06 22:52:04 +0530

    bbasic details added

--- src/App.js
+++ src/App.js
@@ -2,7 +2,28 @@ import React from 'react';
 
 function App() {
   return (
-    <h1>Hello, world!</h1>
+    <div className="max-w-sm rounded overflow-hidden shadow-lg">
+      <img src="https://source.unsplash.com/random" alt="" className="w-full" />
+      <div className='px-6 py-4'>
+        <div className='font-bold text-purple-500 text-xl mb-2'>
+          Photo by John Doe 
+        </div>
+        <ul>
+          <li>
+            <strong>Views:</strong>
+            4000
+          </li>
+          <li>
+            <strong>Downloads:</strong>
+            300
+          </li>
+          <li>
+            <strong>Likes:</strong>
+            400
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 }
 
